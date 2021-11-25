16d4bad9d2d6a8fc1d4e6e8c0f47bd9964a982dc
Lucas Cardoso <lucascardosopsd@gmail.com>
2021-11-25 19:23:46 -0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
+import { Header } from './components/Header';
+import { Home } from './pages/Home';
+import logo from '@assets/logo.png'
 import './global.scss'
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+      <Header logo={logo} menuItems={[
+        {
+          text: "HOME",
+          link: "#homeSection"
+        },
+        {
+          text: "TOKENOMICS",
+          link: "#tokenomicsSection"
+        },
+        {
+          text: "ROADMAP",
+          link: "#roadmapSection"
+        },
+        {
+          text: "HOW IT WORKS",
+          link: "#howWorksSection"
+        },
+        {
+          text: "WHITE PAPER",
+          link: "#homeSection"
+        },
+        {
+          text: "THE CHART",
+          link: "#homeSection"
+        },
+        {
+          text: "OUR TEAM",
+          link: "#ourTeamSection"
+        },
+        {
+          text: "FAQ",
+          link: "#faqSection"
+        },
+        {
+          text: "BLOG",
+          link: "#"
+        },
+      ]}/>
+      <Home/>
+    </>
   );
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,48 @@
+import {FiMenu} from 'react-icons/fi'
+import {AiOutlineClose} from 'react-icons/ai'
+
+import styles from './styles.module.scss'
+import { useState } from 'react'
+
+interface MenuItem{
+    text: string
+    link: string
+}
+
+interface HeaderProps{
+    logo: string
+    menuItems: MenuItem[]
+}
+
+export function Header({logo, menuItems}: HeaderProps){
+
+    const [isMenuVisible, setMenuVisibility] = useState(false)
+
+    return(
+        <>
+            <nav className={styles.navbar}>
+                <div>
+                    <a href="#homeSection">
+                        <img src={logo} alt="" />
+                    </a>
+                    <ul className={!isMenuVisible ? styles.hidden : ''}>
+                        {
+                            menuItems.map((item: MenuItem) => (
+                                <li key={item.text}>
+                                    <a href={item.link}>
+                                        {item.text}
+                                    </a>
+                                </li>
+                            ))
+                        }
+                        <AiOutlineClose 
+                        onClick={() => setMenuVisibility(false)}
+                        className={styles.closeIcon} color="#fff"
+                        />
+                    </ul>
+                    <FiMenu onClick={() => setMenuVisibility(true)} />
+                </div>
+            </nav>
+        </>
+    )
+}
