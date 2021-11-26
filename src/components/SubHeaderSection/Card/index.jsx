1fbe227921a0efb3731e0483b9dc916a94504095
Lucas Cardoso <lucascardosopsd@gmail.com>
2021-11-26 20:30:34 -0300

    subheader section

--- src/components/SubHeaderSection/Card/index.jsx
+++ src/components/SubHeaderSection/Card/index.jsx
@@ -1,18 +1,21 @@
+import styles from './styles.module.scss'
 
-export function Card({title, text, cardIcon, iconFirst}){
+export function Card({title, text, iconClass, iconFirst}){
 
     return(
-        <div>
+        <div className={styles.card}>
 
             {
-                iconFirst && <img src={cardIcon} alt="" />
+                !iconFirst && <span className={iconClass}></span>
             }
 
-            <h1>{title}</h1>
-            <p>{text}</p>
+            <div>
+                <h1>{title}</h1>
+                <p>{text}</p>
+            </div>
 
             {
-                !iconFirst && <img src={cardIcon} alt="" />
+                iconFirst && <span className={iconClass}></span>
             }
 
         </div>
--- src/components/SubHeaderSection/index.jsx
+++ src/components/SubHeaderSection/index.jsx
@@ -1,5 +1,6 @@
 import { Card } from './Card'
 import farmIcon from '@assets/farm.svg'
+import logo from '@assets/logo.png'
 import styles from './styles.module.scss'
 
 export function SubHeaderSection(){
@@ -7,12 +8,44 @@ export function SubHeaderSection(){
     return(
         <section className={styles.subHeaderSection}>
 
-            <div>
-                <Card title="TELEGRAM GROUP" 
-                text="Join our Telegram group for all the latest updates on 
-                Mari token and read more about our long term plan."
-                cardIcon={farmIcon}
-                />
+            <div className={styles.container}>
+                <div className={styles.cardSection}>
+                    <Card title="TELEGRAM GROUP" 
+                    text="Join our Telegram group for all the latest updates on 
+                    Mari token and read more about our long term plan."
+                    iconClass="icon-agriculture"
+                    />
+                    
+                    <span/>
+
+                    <Card title="TELEGRAM GROUP" 
+                    text="Join our Telegram group for all the latest updates on 
+                    Mari token and read more about our long term plan."
+                    iconClass="icon-farm"
+                    />
+                </div>
+
+                <div className={styles.centerImg}>
+                    <img src={logo} alt="" />
+                </div>
+
+                <div className={styles.cardSection}>
+                    <Card title="TELEGRAM GROUP" 
+                    text="Join our Telegram group for all the latest updates on 
+                    Mari token and read more about our long term plan."
+                    iconClass="icon-oil"
+                    iconFirst
+                    />
+                    
+                    <span/>
+
+                    <Card title="TELEGRAM GROUP" 
+                    text="Join our Telegram group for all the latest updates on 
+                    Mari token and read more about our long term plan."
+                    iconClass="icon-shovels"
+                    iconFirst
+                    />
+                </div>
             </div>
 
         </section>
