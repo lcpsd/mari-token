f16e3d3ff6af5b332b21bf8ad9dc12b9667ecdc1
Lucas Cardoso <lucascardosopsd@gmail.com>
2021-11-26 20:35:22 -0300

    subheader real text

--- src/components/SubHeaderSection/index.jsx
+++ src/components/SubHeaderSection/index.jsx
@@ -18,9 +18,8 @@ export function SubHeaderSection(){
                     
                     <span/>
 
-                    <Card title="TELEGRAM GROUP" 
-                    text="Join our Telegram group for all the latest updates on 
-                    Mari token and read more about our long term plan."
+                    <Card title="TOKEN PROTOCOL" 
+                    text="6% of each transaction is split among token holders and the burn wallet rewarding holders."
                     iconClass="icon-farm"
                     />
                 </div>
@@ -30,18 +29,16 @@ export function SubHeaderSection(){
                 </div>
 
                 <div className={styles.cardSection}>
-                    <Card title="TELEGRAM GROUP" 
-                    text="Join our Telegram group for all the latest updates on 
-                    Mari token and read more about our long term plan."
+                    <Card title="CONTESTS" 
+                    text="Giveaways will be held once a week with flash and happy hour Giveaways introduced along the way!"
                     iconClass="icon-oil"
                     iconFirst
                     />
                     
                     <span/>
 
-                    <Card title="TELEGRAM GROUP" 
-                    text="Join our Telegram group for all the latest updates on 
-                    Mari token and read more about our long term plan."
+                    <Card title="TECHRATE AUDIT" 
+                    text="Fully audited by Techrate, our Smart Contract does not contain any severe vulnerabilities."
                     iconClass="icon-shovels"
                     iconFirst
                     />
